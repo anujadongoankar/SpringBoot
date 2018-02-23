63763c11e3cf97e6eb30568bf85f6d8d0683e93f
anuja <anujadongoankar@gmail.com>
2018-02-22 20:53:42 -0500

    added gitignore

---
+++ src/main/java/com/InRhythm/main/Greeter.java
@@ -0,0 +1,9 @@
+package com.InRhythm.main;
+
+public class Greeter {
+
+    public String sayHello() {
+        return "Hello world!";
+    }
+
+}
---
+++ src/main/java/com/InRhythm/main/Hello.java
@@ -0,0 +1,9 @@
+package com.InRhythm.main;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
