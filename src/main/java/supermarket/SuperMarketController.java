b0c08b8b956b9e13e8f8cf76b71b2e42fc3c54f2
anuja <anujadongoankar@gmail.com>
2018-02-23 07:42:45 -0500

    added retrive products functionality

--- src/main/java/supermarket/SuperMarketController.java
+++ src/main/java/supermarket/SuperMarketController.java
@@ -11,9 +11,15 @@ public class SuperMarketController {
 
 
     @RequestMapping(method = RequestMethod.POST, value = "/products")
-    public String addNewProduct(@RequestBody Product product) {
+    public String products(@RequestBody Product product) {
         productRepository.save(product);
         return "you have added " + product.getName() + " with category: " + product.getCategory() + " Price: " + product.getPrice();
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "/products")
+    public Iterable<Product> products() {
+        return productRepository.findAll();
     }
 
 }
